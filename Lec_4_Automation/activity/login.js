10f1bcb744634248b760c7add274f4e6b739f01c
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2020-12-12 15:26:31 +0530

    got code

--- Lec_4_Automation/activity/login.js
+++ Lec_4_Automation/activity/login.js
@@ -5,6 +5,8 @@ const pw = "Sachin@12345";
 
 const puppeteer = require("puppeteer");
 let gTab;
+let gIdx;
+let gCode;
 // TO Open A Browser
 //by Default true
 // ye Launch ki setting hai sarri object daali hai argument me
@@ -38,20 +40,12 @@ browserOpenPromise.then(function(browser){
 })
 //click => navigation => click
 .then(function(){
-    let waitPromise = gTab.waitForSelector(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled", {visible : true});
-    return waitPromise;
+    let waitAndClickPromise = waitAndClick(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled");
+    return waitAndClickPromise;
 })
 .then(function(){
-    let ipKitClickedPromise = gTab.click(".ui-btn.ui-btn-normal.ui-btn-large.ui-btn-primary.ui-btn-link.ui-btn-styled");
-    return ipKitClickedPromise;
-})
-.then(function(){
-    let waitPromise = gTab.waitForSelector(".ui-btn.ui-btn-normal.playlist-card-btn.ui-btn-primary.ui-btn-link.ui-btn-styled", {visible : true});
-    return waitPromise;
-})
-.then(function(){
-    let seeChalengesClickedPromise = gTab.click(".ui-btn.ui-btn-normal.playlist-card-btn.ui-btn-primary.ui-btn-link.ui-btn-styled", {visible : true});
-    return seeChalengesClickedPromise;
+    let waitAndClickPromise = waitAndClick(".ui-btn.ui-btn-normal.playlist-card-btn.ui-btn-primary.ui-btn-link.ui-btn-styled");
+    return waitAndClickPromise;
 })
 .then(function(){
     let waitPromise = gTab.waitForSelector(".js-track-click.challenge-list-item", {visible : true});
@@ -72,11 +66,106 @@ browserOpenPromise.then(function(browser){
     return pendingPromiseOfAllLinks;
 })
 .then(function(allLinks){
+    console.log(allLinks);
     let completeLink = allLinks.map(function(link){
         return  `https://www.hackerrank.com${link}`;
     })
     console.log(completeLink);
+    let oneQuesPendingPromise = solveQuestion(completeLink[0]);
+        return oneQuesPendingPromise;
+})
+.then(function(){
+    console.log("One Question Solved Successfully");
 })
 .catch(function(error){
-    console.log(error);
+    reject(error);
 })
+
+function waitAndClick(selector){
+    return new Promise(function(resolve, reject){
+        let waitPromise = gTab.waitForSelector(selector, {visible : true});
+        waitPromise.then(function(){
+            let clickPromise = gTab.click(selector);
+            return clickPromise;
+        })
+        .then(function(){
+            resolve();
+        })
+        .catch(function(error){
+            reject(error);
+        })
+    });
+}
+function getCode(){
+    return new Promise( function(resolve, reject){
+        let waitPromise = gTab.waitForSelector(".hackdown-content h3", {visible: true});
+        waitPromise.then(function(){
+            let allCodeNamesElementsPromise = gTab.$$(".hackdown-content h3");
+            return allCodeNamesElementsPromise; 
+        })
+        .then(function(allCodeNamesElement){
+            //[ <h3>C++</h3>, <h3>Java</h3>, <h3>Pythonh3>]
+            // allCodeNamePromise = [ Promise{pending}, Promise{pending}, Promise{pending}]
+            let allCodeNamePromise = [];
+            for(let i = 0; i < allCodeNamesElement.length; i++){
+                let namePromise = gTab.evaluate( function(elem){ return elem.textContent;  }, allCodeNamesElement[i] );
+                allCodeNamePromise.push(namePromise);
+            }
+            let promiseOfAllCodesNames = Promise.all(allCodeNamePromise);
+            return promiseOfAllCodesNames;
+        })
+        .then(function(codeNames){
+            //[C++, Java, Python];
+            let idx;
+            for(let i = 0; i < codeNames.length; i++){
+                if(codeNames[i] == "C++"){
+                    idx = i;
+                    break;
+                }
+            }
+            gIdx = idx;
+            let allCodesElementPromise = gTab.$$(".hackdown-content .highlight");
+            return allCodesElementPromise;
+        })
+        .then(function(allCodeElement){
+            //[ <div></div>, <div></div>, <div></div> ]
+            let codeDiv = allCodeElement[gIdx];
+            let codePromise = gTab.evaluate( function(elem){ return elem.textContent;  }, codeDiv)
+            return codePromise;
+
+        })
+        .then(function(code){
+            console.log(code);
+            gCode = code;
+            resolve();
+        })
+        .catch(function(){
+            reject(error);
+        })
+    });
+}
+function solveQuestion(quesLink){
+    return new Promise(function(resolve, reject){
+         let quesGotoPromise = gTab.goto(quesLink);
+         quesGotoPromise.then(function(){
+            let waitAndClickPromise = waitAndClick('div[data-attr2="Editorial"]');
+            return waitAndClickPromise;
+        })
+        /*
+        .then(function(){
+            let waitAndClickPromise = waitAndClick(".ui-btn.ui-btn-normal.ui-btn-primary.ui-btn-styled ");
+            return waitAndClickPromise;
+        }) */
+        .then(function(){
+            let codePromise = getCode();
+            return codePromise;
+        })
+        .then(function(){
+            console.log("Got Code");
+        })
+        .catch(function(error){
+           // console.log(error);
+            reject(error);
+        })
+    });
+}
