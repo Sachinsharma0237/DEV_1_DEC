5e5de342c65d5f0113221ff80b16adb525573dcf
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2020-12-21 20:46:31 +0530

    address val set

--- Lec_6_Electron_Excel/js/script.js
+++ Lec_6_Electron_Excel/js/script.js
@@ -1,1 +1,17 @@
 //Node Features ==> It won't work in web, but it'll work fine standalone
+let $ = require("jquery");
+
+$(document).ready(function(){
+
+    console.log("Document is Loaded!!!");
+    $(".cell").on("click", function(){
+        console.log(this);
+        let rowId = Number($(this).attr("rowId"));
+        let colId = Number($(this).attr("colId"));
+        // console.log(rowId);
+        // console.log(colId);
+        let address = String.fromCharCode(65+colId) + rowId + "";
+        console.log(address);
+        $("#address").val(address);
+    });
+})
