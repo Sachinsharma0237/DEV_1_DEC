8400b231062415a462695213c23dbfd5a8995100
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2021-01-03 02:04:52 +0530

    excel clone completed

--- Lec_6_Electron_Excel/main.js
+++ Lec_6_Electron_Excel/main.js
@@ -1,16 +1,31 @@
-//Electron Logic
+// inside electron excel folder 
+// create a main.js
+// npm init -y => package.json file is created
+// npm i electron
+// in package.json file under scripts add "start":"electron ."
 
-const { app, BrowserWindow } = require('electron');
+// npm i ejs-electron
+
+// electron logic
+
+// object destructuring
+const { app, BrowserWindow } = require('electron')
 const ejse = require("ejs-electron");
 
+// const electron = require("electron");
+// const app = electron.app;
+// const BrowserWindow = electron.BrowserWindow;
+
+
 function createWindow () {
-  const win = new BrowserWindow({
+const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true, // node enable
+      enableRemoteModule:true
     }
-  })
+  });
 
   win.loadFile('index.ejs').then(function(){
       win.maximize();
